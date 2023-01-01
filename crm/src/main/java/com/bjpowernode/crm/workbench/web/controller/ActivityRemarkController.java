60cf96d3d2584ee954b9d4b54e9019eed3e7febf
wzh <13841680116@163.com>
2023-01-01 21:07:29 +0800

    1.0.6

--- crm/src/main/java/com/bjpowernode/crm/workbench/web/controller/ActivityRemarkController.java
+++ crm/src/main/java/com/bjpowernode/crm/workbench/web/controller/ActivityRemarkController.java
@@ -7,7 +7,6 @@ import com.bjpowernode.crm.commons.utils.UUIDUtils;
 import com.bjpowernode.crm.settings.domain.User;
 import com.bjpowernode.crm.workbench.domain.ActivityRemark;
 import com.bjpowernode.crm.workbench.service.ActivityRemarkService;
-import org.aspectj.apache.bcel.classfile.Constant;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
